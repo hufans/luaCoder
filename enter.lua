fb3dade13ff671135ff1fb76a18f6931c8c99f58
fan <434880565@qq.com>
2018-04-10 13:57:52 +0800

    666

--- enter.lua
+++ enter.lua
@@ -2,13 +2,11 @@ local breakSocketHandle,debugXpCall
 breakSocketHandle,debugXpCall = require("LuaDebugjit")("localhost",7003)
 
 
-local tb = {[1]= 3,[3] = 6,[7] = 8}
-
-function function_name( tb )
-   local x = 0 ;
-   for k,v in pairs(tb) do
-        x = x+v;
-   end
-   print(x)
+function f1(n)
+    if n>0 then
+        return n + f1(n-1)
+    else
+        return 0
+    end
 end
-function_name(tb)
+print(f1(2018))
